ef6593472d11181850bcd597a822ca3684267ab7
Abdellah IDRISSI AZAMI <idrissi.azami.abdellah@gmail.com>
2023-09-20 09:50:25 +0100

    commit 3
    model creation

---
+++ Data/HPOModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Class1
+{
+	public Class1()
+	{
+		public int ID {  get; set; }
+		[Required]
+		[Display(Name ="HPO Term")]
+		public string Term { get; set; }
+		[Required]
+		[Display(Name = "HPO ID")]
+		public string HPOID { get; set; }
+
+	}
+}
